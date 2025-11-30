fe879f6edb5ada8197c4ac9a130b65ea2e6b702d
Kula Prakash <147312044+SHADOWLORD6417@users.noreply.github.com>
2025-11-30 14:58:49 +0530

    Working Updated

--- frontend/src/Components/Navigation/Navigation.js
+++ frontend/src/Components/Navigation/Navigation.js
@@ -11,8 +11,8 @@ function Navigation({active, setActive}) {
             <div className="user-con">
                 <img src={avatar} alt="" />
                 <div className="text">
-                    <h2>Mike</h2>
-                    <p>Your Money</p>
+                    <h2>KULA</h2>
+                    <p></p>
                 </div>
             </div>
             <ul className="menu-items">
